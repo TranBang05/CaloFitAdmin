fa53d2af062ce8abbd7dd35c7a33144d970fc676
Tranbang <tranxuanbang.yt@gmail.com>
2024-06-15 05:41:46 +0700

    update appsetting

--- obj/Debug/net6.0/AdminApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/AdminApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0453dd54b2f92f1b6486775197f3c06e218c3bff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
